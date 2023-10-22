4c920e6892d6044370d5ec33bec194bd0caaa800
gabi <144284968+ninikx@users.noreply.github.com>
2023-10-22 18:50:11 +0200

    wrong colors atm

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,27 @@ pub fn run(image_width: u32) {
     let aspect_ratio = 16.0 / 9.0;
     let image_height = (image_width as f64 / aspect_ratio) as u32;
 
+    // Image height should be at least 1
+    let image_height = if image_height < 1 { 1 } else { image_height };
+
     // Camera
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * image_width as f64 / image_height as f64;
+    let camera_center = Point3::origin();
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Calculate the location of the upper left pixel.
+    let viewport_upper_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     let origin = Point3::origin();
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
@@ -29,11 +46,12 @@ pub fn run(image_width: u32) {
     for j in (0..=image_height - 1).rev() {
         eprintln!("\rScanlines remaining: {}", j);
         for i in 0..image_width {
-            let u = i as f64 / (image_width - 1) as f64;
-            let v = j as f64 / (image_height - 1) as f64;
+            let pixel_center =
+                pixel00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
             let r = Ray {
-                origin,
-                direction: lower_left_corner + u * horizontal + v * vertical - origin,
+                origin: camera_center,
+                direction: ray_direction,
             };
             print!("{}", r.ray_color());
         }
